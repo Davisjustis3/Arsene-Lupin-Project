baf3b7a6c661a84472d90f7fe26ac6560bfc5a53
Justis <davisjustis3@gmail.com>
2024-12-29 03:52:27 +0100

    Cleaning up and adding confidence function-display

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let daysLeft = 11;
 // Confidence is the ability to comeplete risky tasks
 // We need 150 confidence to complete the saxophone heist 
 let confidence = 0;
+
 console.log(confidence);
 let inventory = [];
 
@@ -27,16 +28,19 @@ const textDisplay = document.querySelector('.text-display')
 const statusBar = document.querySelector('.status-bar')
 const hideout = document.querySelector('.hideout');
 const quitJobSideButton = document.querySelector('.quit');
+const inventoryContainer = document.querySelector('.inventory')
 const inventoryText = document.querySelector('.inventory-text')
 const inv1 = document.querySelector('.inv1');
 const inv2 = document.querySelector('.inv2');
 const inv3 = document.querySelector('.inv3');
 const inv4 = document.querySelector('.inv4');
+const confidenceContainer = document.getElementById('confidence-container');
 
 
-
+inventoryContainer.style.display = 'none';
 submitButton.addEventListener("click", startGame);
 function startGame() {
+  
   if (
     inputBar.value == 'Yes I accept' ||
     inputBar.value == 'yes I accept' ||
@@ -55,6 +59,7 @@ function startGame() {
       openingStatement();
       inputBar.value = '';
   };
+
 };
 function openingStatement() {
    setTimeout(function () { 
@@ -67,16 +72,19 @@ function acceptance() {
   submitButton.removeEventListener("click", startGame);
   submitButton.addEventListener("click", details);
   inputBar.value = '';
+  inputBar.style.display = 'none'
 };
 function details() {
   textDisplay.innerText = "SO... Here are the details. John Coltrane's saxophone is sitting in the owner's office of Jo's Jazz Club. I have a buyer and I need you to get it for me before the purchase date. I don't care how you do it but don't damage anything on the saxophone. Once you have it, leave it at the footsteps of the Ministry Hall. A failure will never be forgotten. That is all. Click 'NEXT' to begin.";
   submitButton.innerHTML = 'NEXT';
   submitButton.removeEventListener("click", details);
   submitButton.addEventListener("click", hideoutHome);
-  inputBar.value = '';
+
 }
 
 function hideoutHome() {
+  confidenceContainer.innerHTML = `Confidence: ${confidence}`
+  inventoryContainer.style.display = 'block';
   inputBar.value = '';
   hideout.style.display = 'none';
   statusBar.style.display = 'flex';
@@ -86,12 +94,12 @@ function hideoutHome() {
   btn6.style.display = 'none';
   btn7.style.display = 'none';
   btn8.style.display = 'none';
-  textDisplay.innerText = "You are in your secret hideout in Paris. Here, you can plan your next heist or go complete small tasks, like going to the store to buy supplies. Hint** you can also steal them. But don't get caught ;). Each tasks gains your confidence and tasks become easier. Add items to your inventory by buying them in the store, but you can only carry 3 things at one time. You'll need certain items to perform specific tasks, so think before you buy. There are three ways to get in: As a janitor, a band member, or a potential buyer. Think critically and good luck!"
+  textDisplay.innerText = "You are in your secret hideout in Paris. Here, you can plan your next heist or go complete small tasks, like going to the store to buy supplies. Hint** you can also steal them. But don't get caught ;). Each tasks gains your confidence and tasks become easier. Add items to your inventory by buying them in the store, but you can only carry 3 things at one time. You'll need certain items to perform specific tasks, so think before you buy. There are three ways to get in: As a janitor, a band member, or a potential buyer. Think critically and good luck!";
   btn1.innerHTML = 'Go to store';
   btn2.innerHTML = 'Go to center';
   btn3.innerHTML = 'Go to club';
   btn4.innerHTML = 'Quit Job';
-  submitButton.onclick = undefined;
+  submitButton.style.display = 'none';
   btn1.onclick = goToStore;
   btn2.onclick = goToCenter;
   btn3.onclick = goToClub;
@@ -121,7 +129,7 @@ function goToStore() {
   btn5.onclick = stealWig;
   btn6.onclick = stealClothes;
   btn7.onclick = stealSuit;
-  btn8.onclick = stealSax;
+  btn8.onclick = stealReplacementSax;
   hideout.onclick = hideoutHome;
   quitJobSideButton.onclick = quitJob;
 }
@@ -145,6 +153,191 @@ function goToCenter() {
 function goToClub() {
 
 }
+
+function buyWig() {
+  const wig = 10;
+  if (!inventory.includes('Wig') && euros >= wig && inventory.length < 3) {
+    inventory.push('Wig');
+    confidence += 10;
+    addConfidence();
+    euros -= 10;
+    eurosStatus.innerText = `Euros: ${euros}`
+    inv1.style.display = 'block';
+    inv1.innerText = 'Wig';
+    textDisplay.innerText = 'A wig... interesting choice.';
+  } else if (inventory.includes('Wig')) {
+    textDisplay.innerText = 'You already have a wig you dummy!';
+  } else if (euros < wig) {
+    textDisplay.innerText = 'You need to go find some money... Or just steal the wig, but I hope you have the confidence to pull it off. ;)';
+  } if (inventory.length >= 3) {
+    textDisplay.innerText = "You can't get anything else. Sorry.";
+    mainBtn.onclick = hideoutHome;
+    stealBtn.onclick = hideoutHome;
+  }
+}
+function buyClothes() {
+  const janitorClothes = 50
+  if (!inventory.includes('Janitor Clothes') && euros >= janitorClothes && inventory.length < 3) {
+    inventory.push('Janitor Clothes');
+    confidence += 20;
+    addConfidence();
+    euros -= 50;
+    eurosStatus.innerText = `Euros: ${euros}`
+    textDisplay.innerText = 'Nice, you got the uniform! Hope it comes in handy';
+    inv2.style.display = 'block';
+    inv2.innerText = 'Janitor Clothes';
+  } else if (inventory.includes('Janitor Clothes')) {
+    textDisplay.innerText = 'You already have a uniform you dummy!';
+  } else if (euros < janitorClothes) {
+    textDisplay.innerText = 'You need to go find some money... Or just steal the uniform, but I hope you have the confidence to pull it off. ;)';
+  } 
+  if (inventory.length >= 3) {
+    textDisplay.innerText = "You can't get anything else. Sorry.";
+    mainBtn.onclick = hideoutHome;
+    stealBtn.onclick = hideoutHome;
+  }
+}
+function buySuit() {
+  const suit = 50
+  if (!inventory.includes('Suit') && euros >= suit && inventory.length < 3) {
+    inventory.push('Suit');
+    confidence += 20;
+    addConfidence();
+    euros -= 50;
+    eurosStatus.innerText = `Euros: ${euros}`
+    textDisplay.innerText = 'You clean up very nice, sir. That is a fine suit.';
+    inv3.style.display = 'block';
+    inv3.innerText = 'Suit';
+  } else if (inventory.includes('Suit')) {
+    textDisplay.innerText = 'You already have a suit you dummy!';
+  } else if (euros < suit) {
+    textDisplay.innerText = 'You need to go find some money... Or just steal the suit, but I hope you have the confidence to pull it off. ;)';
+  } 
+  if (inventory.length >= 3) {
+    textDisplay.innerText = "You can't get anything else. Sorry.";
+    mainBtn.onclick = hideoutHome;
+    stealBtn.onclick = hideoutHome;
+  }
+}
+function buySax() {
+  const sax = 250
+  if (!inventory.includes('Saxophone') && euros >= sax && inventory.length < 3) {
+    inventory.push('Saxophone');
+    confidence += 50;
+    addConfidence();
+    euros -= 250;
+    eurosStatus.innerText = `Euros: ${euros}`
+    textDisplay.innerText = "You're going to need a lot of practice to make that thing sound good.";
+    inv4.style.display = 'block';
+    inv4.innerText = 'Saxophone';
+  } else if (inventory.includes('Saxophone')) {
+    textDisplay.innerText = 'You already have a saxophone you dummy!';
+  } else if (euros < suit) {
+    textDisplay.innerText = 'You need to go find some money... Or just steal the sax, but I hope you have the confidence to pull it off. ;)';
+  } 
+  if (inventory.length >= 3) {
+    textDisplay.innerText = "You can't get anything else. Sorry.";
+    mainBtn.onclick = hideoutHome;
+    stealBtn.onclick = hideoutHome;
+  }
+}
+let result;
+function stealIt() {
+  if (confidence <= 20) {
+      chances = Math.random()*20;
+    if (chances <= 8) {
+      textDisplay.innerText = "Wow! You stole it. Good job!";
+      allBtn.style.display = 'none';
+      result = 'success';
+    } else {
+      textDisplay.innerText = "Wow, you idiot. You got caught."
+      allBtn.style.display = 'none';
+      confidence -= 50;
+      addConfidence();
+      result = 'failure';
+    }
+    console.log(result);
+    return result;
+  } else if (confidence > 20 && confidence <= 40) {
+      chances = Math.random()*15;
+    if (chances <= 8) {
+      textDisplay.innerText = "Wow! You stole it. Good job!";
+      allBtn.style.display = 'none';
+      result = 'success';
+    } else {
+      textDisplay.innerText = "Wow, you idiot. You got caught."
+      allBtn.style.display = 'none';
+      confidence -= 50;
+      addConfidence();
+       result = 'failure';
+    } 
+  } else if (confidence > 40 && confidence <= 100) {
+      chances = Math.random()*10;
+    if (chances <= 8) {
+      textDisplay.innerText = "Wow! You stole it. Good job!";
+      allBtn.style.display = 'none';
+      result = 'success';
+    } else {
+      textDisplay.innerText = "Wow, you idiot. You got caught."
+      allBtn.style.display = 'none';
+      confidence -= 50;
+      addConfidence();
+      result = 'failure';
+    } 
+  }
+}
+function stealWig() {
+  hideout.onclick = hideoutHome;
+  if (!inventory.includes('Wig') && inventory.length < 3) {
+    stealIt();
+    inventory.push('Wig');
+    confidence += 40;
+    addConfidence();
+    inv1.style.display = 'block';
+    inv1.innerText = 'Wig';
+  } else if (inventory.includes('Wig')) {
+    textDisplay.innerText = "Think... you don't need the wig. You already have one. So don't risk it.";
+  } 
+}
+function stealClothes() {
+  hideout.onclick = hideoutHome;
+  if (!inventory.includes('Janitor Clothes') && inventory.length < 3) {
+    stealIt();
+    inventory.push('Janitor CLothes');
+    confidence += 40;
+    addConfidence();
+    inv2.style.display = 'block';
+    inv2.innerText = 'Janitor Clothes ';
+  } else if (inventory.includes('Janitor Clothes')) {
+    textDisplay.innerText = "Do you have two bodies? NO Why do yu need two janitor uniforms. Go home, Rodger!!";
+  } 
+}
+function stealSuit() {
+  hideout.onclick = hideoutHome;
+  if (!inventory.includes('Suit') && inventory.length < 3) {
+    stealIt();
+    inventory.push('Suit');
+    confidence += 60;
+    addConfidence();
+    inv3.style.display = 'block';
+    inv3.innerText = 'Suit';
+  } else if (inventory.includes('Suit')) {
+    textDisplay.innerText = "Does it hurt to have two suits? Not really, but NO you don't need another one. Go steal something else!";
+  } 
+}
+function stealReplacementSax() {
+  hideout.onclick = hideoutHome;
+  if (!inventory.includes('Sax') && inventory.length < 3) {
+    stealIt();
+    inventory.push('Sax');
+    confidence += 60;
+    addConfidence();
+    inv4.style.display = 'block';
+    inv4.innerText = 'Sax';
+  } else if (inventory.includes('Sax')) {
+    textDisplay.innerText = "I'm blown away by your ambition, but be SMART ya dummy. You don't need another sax. Go somewhere else.";
+  } 
+}
 function gameOver() {
   textDisplay.innerText = "GAME OVER";
   inputBar.style.display = 'none';
@@ -162,194 +355,6 @@ function quitJob() {
   allBtn.style.display = 'none';
   textDisplay.innerText = 'I hope you understand that quitting means trying your luck with DEATH. Surely you can run, but we WILL find you.'
 }
-function buyWig() {
-  const wig = 10;
-  if (!inventory.includes('Wig') && euros >= wig && inventory.length < 3) {
-    inventory.push('Wig');
-    confidence += 10;
-    euros -= 10;
-    eurosStatus.innerText = `Euros: ${euros}`
-    inv1.style.display = 'block';
-    inv1.innerText = 'Wig';
-    textDisplay.innerText = 'A wig... interesting choice.';
-  } else if (inventory.includes('Wig')) {
-    textDisplay.innerText = 'You already have a wig you dummy!';
-  } else if (euros < wig) {
-    textDisplay.innerText = 'You need to go find some money... Or just steal the wig, but I hope you have the confidence to pull it off. ;)';
-  } if (inventory.length >= 3) {
-    textDisplay.innerText = "You can't get anything else. Sorry.";
-    mainBtn.onclick = hideoutHome;
-    stealBtn.onclick = hideoutHome;
-  }
-  console.log(euros);
-  console.log(inventory);
-  console.log(confidence);
-}
-function buyClothes() {
-  const janitorClothes = 50
-  if (!inventory.includes('Janitor Clothes') && euros >= janitorClothes && inventory.length < 3) {
-    inventory.push('Janitor Clothes');
-    confidence += 20;
-    euros -= 50;
-    eurosStatus.innerText = `Euros: ${euros}`
-    textDisplay.innerText = 'Nice, you got the uniform! Hope it comes in handy';
-    inv2.style.display = 'block';
-    inv2.innerText = 'Janitor Clothes';
-  } else if (inventory.includes('Janitor Clothes')) {
-    textDisplay.innerText = 'You already have a uniform you dummy!';
-  } else if (euros < janitorClothes) {
-    textDisplay.innerText = 'You need to go find some money... Or just steal the uniform, but I hope you have the confidence to pull it off. ;)';
-  } 
-  if (inventory.length >= 3) {
-    textDisplay.innerText = "You can't get anything else. Sorry.";
-    mainBtn.onclick = hideoutHome;
-    stealBtn.onclick = hideoutHome;
-  }
-  console.log(euros);
-  console.log(inventory);
-  console.log(confidence);
-
-}
-function buySuit() {
-  const suit = 50
-  if (!inventory.includes('Suit') && euros >= suit && inventory.length < 3) {
-    inventory.push('Suit');
-    confidence += 20;
-    euros -= 50;
-    eurosStatus.innerText = `Euros: ${euros}`
-    textDisplay.innerText = 'You clean up very nice, sir. That is a fine suit.';
-    inv3.style.display = 'block';
-    inv3.innerText = 'Suit';
-  } else if (inventory.includes('Suit')) {
-    textDisplay.innerText = 'You already have a suit you dummy!';
-  } else if (euros < suit) {
-    textDisplay.innerText = 'You need to go find some money... Or just steal the suit, but I hope you have the confidence to pull it off. ;)';
-  } 
-  if (inventory.length >= 3) {
-    textDisplay.innerText = "You can't get anything else. Sorry.";
-    mainBtn.onclick = hideoutHome;
-    stealBtn.onclick = hideoutHome;
-  }
-  console.log(euros);
-  console.log(inventory);
-  console.log(confidence);
-}
-function buySax() {
-  const sax = 250
-  if (!inventory.includes('Saxophone') && euros >= sax && inventory.length < 3) {
-    inventory.push('Saxophone');
-    confidence += 50;
-    euros -= 250;
-    eurosStatus.innerText = `Euros: ${euros}`
-    textDisplay.innerText = "You're going to need a lot of practice to make that thing sound good.";
-    inv4.style.display = 'block';
-    inv4.innerText = 'Saxophone';
-  } else if (inventory.includes('Saxophone')) {
-    textDisplay.innerText = 'You already have a saxophone you dummy!';
-  } else if (euros < suit) {
-    textDisplay.innerText = 'You need to go find some money... Or just steal the sax, but I hope you have the confidence to pull it off. ;)';
-  } 
-  if (inventory.length >= 3) {
-    textDisplay.innerText = "You can't get anything else. Sorry.";
-    mainBtn.onclick = hideoutHome;
-    stealBtn.onclick = hideoutHome;
-  }
-  console.log(euros);
-  console.log(inventory);
-  console.log(confidence);
-}
-let result;
-function stealIt() {
-  if (confidence <= 20) {
-      chances = Math.random()*20;
-    if (chances <= 8) {
-      textDisplay.innerText = "Wow! You stole it. Good job!";
-      allBtn.style.display = 'none';
-      result = 'success';
-    } else {
-      textDisplay.innerText = "Wow, you idiot. You got caught."
-      allBtn.style.display = 'none';
-      confidence -= 50;
-      result = 'failure';
-    }
-    console.log(result);
-    return result;
-  } else if (confidence > 20 && confidence <= 40) {
-      chances = Math.random()*15;
-    if (chances <= 8) {
-      textDisplay.innerText = "Wow! You stole it. Good job!";
-      allBtn.style.display = 'none';
-      result = 'success';
-    } else {
-      textDisplay.innerText = "Wow, you idiot. You got caught."
-      allBtn.style.display = 'none';
-      confidence -= 50;
-       result = 'failure';
-    } 
-    console.log(result);
-
-  } else if (confidence > 40 && confidence <= 100) {
-      chances = Math.random()*10;
-    if (chances <= 8) {
-      textDisplay.innerText = "Wow! You stole it. Good job!";
-      allBtn.style.display = 'none';
-      result = 'success';
-    } else {
-      textDisplay.innerText = "Wow, you idiot. You got caught."
-      allBtn.style.display = 'none';
-      confidence -= 50;
-      result = 'failure';
-    } 
-    console.log(result);
-  }
-
-console.log(chances);
-}
-function stealWig() {
-  hideout.onclick = hideoutHome;
-  if (!inventory.includes('Wig') && inventory.length < 3) {
-    stealIt();
-    inventory.push('Wig');
-    confidence += 40;
-    inv1.style.display = 'block';
-    inv1.innerText = 'Wig';
-  } else if (inventory.includes('Wig')) {
-    textDisplay.innerText = "Think... you don't need the wig. You already have one. So don't risk it.";
-  } 
-}
-function stealClothes() {
-  hideout.onclick = hideoutHome;
-  if (!inventory.includes('Janitor Clothes') && inventory.length < 3) {
-    stealIt();
-    inventory.push('Janitor CLothes');
-    confidence += 40;
-    inv2.style.display = 'block';
-    inv2.innerText = 'Janitor Clothes ';
-  } else if (inventory.includes('Janitor Clothes')) {
-    textDisplay.innerText = "Do you have two bodies? NO Why do yu need two janitor uniforms. Go home, Rodger!!";
-  } 
-}
-function stealSuit() {
-  hideout.onclick = hideoutHome;
-  if (!inventory.includes('Suit') && inventory.length < 3) {
-    stealIt();
-    inventory.push('Suit');
-    confidence += 60;
-    inv3.style.display = 'block';
-    inv3.innerText = 'Suit';
-  } else if (inventory.includes('Suit')) {
-    textDisplay.innerText = "Does it hurt to have two suits? Not really, but NO you don't need another one. Go steal something else!";
-  } 
-}
-function stealSax() {
-  hideout.onclick = hideoutHome;
-  if (!inventory.includes('Sax') && inventory.length < 3) {
-    stealIt();
-    inventory.push('Sax');
-    confidence += 60;
-    inv4.style.display = 'block';
-    inv4.innerText = 'Sax';
-  } else if (inventory.includes('Sax')) {
-    textDisplay.innerText = "I'm blown away by your ambition, but be SMART ya dummy. You don't need another sax. Go somewhere else.";
-  } 
+function addConfidence() {
+  confidenceContainer.innerHTML = `Confidence: ${confidence}`
 }
