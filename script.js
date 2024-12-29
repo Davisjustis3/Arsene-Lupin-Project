69bd87d0299115d577f7699f6fadfa95078d89ee
Justis <davisjustis3@gmail.com>
2024-12-29 19:28:53 +0100

    Added render buttons. Building pickpocket functions

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let days = 0;
 let daysLeft = 10;
 // Confidence is the ability to comeplete risky tasks
 // We need 150 confidence to complete the saxophone heist 
-let confidence = 0;
+let confidence = 50;
 
 
 let inventory = [];
@@ -83,16 +83,15 @@ function hideoutHome() {
   statusBar.style.display = 'flex';
   quitJobSideButton.style.display = 'inline';
   allBtn.style.display = 'flex';
-  btn5.style.display = 'none';
-  btn6.style.display = 'none';
-  btn7.style.display = 'none';
-  btn8.style.display = 'none';
+  removeSecondRowButtons();
   textDisplay.style.height = '25vh';
   textDisplay.innerText = "You are in your secret hideout in Paris. Here, you can plan your next heist or go complete small tasks, like going to the store to buy supplies. Hint** you can also steal them. But don't get caught ;). Each tasks gains your confidence and tasks become easier. Add items to your inventory by buying them in the store, but you can only carry 3 things at one time. You'll need certain items to perform specific tasks, so think before you buy. There are three ways to get in: As a janitor, a band member, or a potential buyer. Think critically and good luck!";
   btn1.innerHTML = 'Go to store';
   btn2.innerHTML = 'Go to center';
   btn3.innerHTML = 'Go to club';
+    btn3.style.fontSize = '.8rem'
   btn4.innerHTML = 'Quit Job';
+    btn4.style.fontSize = '.8rem'
   submitButton.style.display = 'none';
   btn1.onclick = goToStore;
   btn2.onclick = goToCenter;
@@ -110,10 +109,7 @@ function goToStore() {
     btn2.style.fontSize = '.8rem';
   btn3.innerHTML = 'Suit 50';
   btn4.innerHTML = 'Sax 250';
-  btn5.style.display = 'inline';
-  btn6.style.display = 'inline';
-  btn7.style.display = 'inline';
-  btn8.style.display = 'inline';
+  renderSecondRowButtons();
   btn5.innerHTML = 'Steal it';
   btn6.innerHTML = 'Steal it';
   btn7.innerHTML = 'Steal it';
@@ -130,6 +126,9 @@ function goToStore() {
   quitJobSideButton.onclick = quitJob;
 }
 function goToCenter() {
+  renderTopRowButtons();
+  removeSecondRowButtons();
+  textDisplay.innerHTML = "You're in the 'streets' of Paris and need to make some quick cash and get proper materials for the heist. Be careful of what you purchase. Some job paths require certain things while others don't.";
   hideout.onclick = hideoutHome;
   hideout.style.display = 'inline';
   quitJobSideButton.style.display = 'inline';
@@ -140,15 +139,100 @@ function goToCenter() {
   btn4.innerHTML = 'Build Fake Profile 200';
     btn4.style.fontSize = '.6rem';
   btn1.onclick = pickpocket;
-  btn2.onclick = cardScam;
-  btn3.onclick = blackMarketCam;
-  btn4.onclick = buildFakeProfile;
-  
-  textDisplay.innerText = "You're in the 'streets' of Paris and need to make some quick cash and get proper materials for the heist. Be careful of what you purchase. Some job paths require certain things while others don't." 
+  // btn2.onclick = cardScam;
+  // btn3.onclick = blackMarketCam;
+  // btn4.onclick = buildFakeProfile;
 }
-function goToClub() {
+function pickpocket() {
+  let confidenceMoral = '';
+  removeTopRowButtons();
+  renderSecondRowButtons();
+  btn5.onclick = touristPick;
+  btn6.onclick = deliveryPick;
+  btn7.onclick = pickpocketAction;
+  btn8.onclick = pickpocketAction;
+  btn5.innerHTML = 'Tourist';
+  btn6.innerHTML = 'Pizza Delivery';
+  btn7.innerHTML = 'Eldery Woman';
+  btn8.innerHTML = 'Pregnant Lady';
+  hideout.onclick = goToCenter;
+  hideout.innerHTML = 'Back';
 
-}
+  if (confidence > 50) {
+    confidenceMoral = 'low -- so beware of who you choose to prey on.';
+  } else if (confidence >= 50 && confidence < 100) {
+    confidenceMoral = 'moderate -- so taking a shot at most of these people will not be a problem.';
+  } else {
+    confidenceMoral = 'high -- so this really should not a be a problem at all.';
+  }
+  textDisplay.innerHTML = `In the world of pickpocketing, it's all about your confidence. By the looks of it -- your confidence level is pretty ${confidenceMoral}
+  You have four general choices and all have their own consequences. Robbing a pregnant lady is a little messed up, and could lower your confidence. But, the payout could be nice. Same with the rest. You only get 3 ATTEMPTS so be smart. Good Luck.
+  Attempts used: ${attempts};
+  `
+};
+let attempts = 0;
+function touristPick() {
+  if (attempts < 3) {
+    chances = Math.random() * 10;
+    if (chances <= 8) {
+      halfDayWork();
+      attempts++;
+      euros += 20;
+      confidence += 10;
+      textDisplay.innerHTML = 'You scored 20 euros and gained some confidence. The tourist has no idea. HAHAHA!'
+      setTimeout(() => {
+        goToCenter();
+        removeSecondRowButtons();
+      }, 6000);
+    } else {
+      attempts++;
+      confidence -= 50;
+      halfDayWork();
+      textDisplay.innerHTML = 'I am not sure if I have chosen the right person... That tourist was ripe for the picking and you failed.'
+      setTimeout(() => {
+        goToCenter();
+        removeSecondRowButtons();
+      }, 6000);
+      }
+  } else if (attempts == 3) {
+    textDisplay.innerHTML = 'Try too many tourists and they will catch on.'
+      setTimeout(() => {
+        goToCenter();
+        removeSecondRowButtons();
+      }, 6000);
+    }
+  }
+function deliveryPick() {
+  if (attempts < 3) {
+    chances = Math.random() * 15;
+    if (chances <= 8) {
+      halfDayWork();
+      attempts++;
+      euros += 40;
+      confidence += 20;
+      textDisplay.innerHTML = 'You scored 20 euros and gained some confidence. The tourist has no idea. HAHAHA!'
+      setTimeout(() => {
+        goToCenter();
+        removeSecondRowButtons();
+      }, 6000);
+    } else {
+      attempts++;
+      confidence -= 50;
+      halfDayWork();
+      textDisplay.innerHTML = 'I am not sure if I have chosen the right person... That tourist was ripe for the picking and you failed.'
+      setTimeout(() => {
+        goToCenter();
+        removeSecondRowButtons();
+      }, 6000);
+      }
+  } else if (attempts == 3) {
+    textDisplay.innerHTML = 'Try too many tourists and they will catch on.'
+      setTimeout(() => {
+        goToCenter();
+        removeSecondRowButtons();
+      }, 6000);
+    }
+  }
 
 function buyWig() {
   const wig = 10;
@@ -156,11 +240,8 @@ function buyWig() {
     inventory.push('Wig');
     displayInventory();
     confidence += 10;
-    displayConfidence();
     euros -= 10;
-    days += .5;
-    daysLeft -= .5;
-    updateStatus();
+    halfDayWork();
     textDisplay.innerText = 'A wig... interesting choice.';
   } else if (inventory.includes('Wig')) {
     textDisplay.innerText = 'You already have a wig you dummy!';
@@ -189,12 +270,9 @@ function buyClothes() {
   if (!inventory.includes('Janitor Clothes') && euros >= janitorClothes && inventory.length < 3) {
     inventory.push('Janitor Clothes');
     displayInventory();
-    confidence += 50;
-    displayConfidence();
+    confidence += 30;
     euros -= 50;
-    days += .5;
-    daysLeft -= .5;
-    updateStatus();
+    halfDayWork();
     textDisplay.innerText = 'Nice, you got the uniform! Hope it comes in handy';
   } else if (inventory.includes('Janitor Clothes')) {
     textDisplay.innerText = 'You already have a uniform you dummy!';
@@ -224,12 +302,9 @@ function buySuit() {
   if (!inventory.includes('Suit') && euros >= suit && inventory.length < 3) {
     inventory.push('Suit');
     displayInventory();
-    confidence += 50;
-    displayConfidence();
+    confidence += 30;
     euros -= 50;
-    days += .5;
-    daysLeft -= .5;
-    updateStatus();
+    halfDayWork();
     textDisplay.innerText = 'You clean up very nice, sir. That is a fine suit.';
   } else if (inventory.includes('Suit')) {
     textDisplay.innerText = 'You already have a suit you dummy!';
@@ -259,12 +334,9 @@ function buySax() {
   if (!inventory.includes('Saxophone') && euros >= sax && inventory.length < 3) {
     inventory.push('Saxophone');
     displayInventory();
-    confidence += 150;
-    displayConfidence();
+    confidence += 50;
     euros -= 250;
-    days += .5;
-    daysLeft -= .5;
-    updateStatus();
+    halfDayWork();
     textDisplay.innerText = "You're going to need a lot of practice to make that thing sound good.";
   } else if (inventory.includes('Saxophone')) {
     textDisplay.innerText = 'You already have a saxophone you dummy!';
@@ -306,19 +378,13 @@ function lowChance() {
       textDisplay.innerText = "Wow! You stole it. Good job!";
       allBtn.style.display = 'none';
       confidence += 40;
-      displayConfidence();
-      days += .5;
-      daysLeft -= .5;
-      updateStatus();
+      halfDayWork();
       return result = true;
     } else if (chances > 8) {
       textDisplay.innerText = "Wow, you idiot. You got caught."
       allBtn.style.display = 'none';
       confidence -= 20;
-      displayConfidence();
-      days += .5;
-      daysLeft -= .5;
-      updateStatus();
+      halfDayWork();
       return result = false;
   };
 };
@@ -327,20 +393,14 @@ function mediumChance() {
     if (chances <= 8) {
       textDisplay.innerText = "Wow! You stole it. Good job!";
       allBtn.style.display = 'none';
-      confidence += 50;
-      displayConfidence();
-      days += .5;
-      daysLeft -= .5;
-      updateStatus();
+      confidence += 30;
+      halfDayWork();
       return result = true;
     } else if (chances > 8) {
       textDisplay.innerText = "Wow, you idiot. You got caught."
       allBtn.style.display = 'none';
       confidence -= 50;
-      displayConfidence();
-      days += .5;
-      daysLeft -= .5;
-      updateStatus();
+      halfDayWork();
       return result = false;
   };
 };
@@ -349,20 +409,14 @@ function highChance() {
     if (chances <= 8) {
       textDisplay.innerText = "Wow! You stole it. Good job!";
       allBtn.style.display = 'none';
-      confidence += 100;
-      displayConfidence();
-      days += .5;
-      daysLeft -= .5;
-      updateStatus();
+      confidence += 80;
+      halfDayWork();
       return result = true;
     } else if (chances > 8) {
       textDisplay.innerText = "Wow, you idiot. You got caught."
       allBtn.style.display = 'none';
       confidence -= 100;
-      displayConfidence();
-      days += .5;
-      daysLeft -= .5;
-      updateStatus();
+      halfDayWork();
       return result = false;
   };
 };
@@ -392,11 +446,43 @@ function displayInventory() {
   inventoryText.innerHTML += inventoryHTML;
 };
 function updateStatus() {
+  confidenceContainer.innerHTML = `Confidence: ${confidence}`
   statusBar.innerHTML = `
       <p class="euros">Euros: ${euros}</p>
       <p class="days">Days: ${days}</p>
       <p class="days-left">Days left: ${daysLeft}</p> `
 };
-function displayConfidence() {
-  confidenceContainer.innerHTML = `Confidence: ${confidence}`
+function renderTopRowButtons() {
+  btn1.style.display = 'inline';
+  btn2.style.display = 'inline';
+  btn3.style.display = 'inline';
+  btn4.style.display = 'inline';
+};
+function removeTopRowButtons() {
+  btn1.style.display = 'none';
+  btn2.style.display = 'none';
+  btn3.style.display = 'none';
+  btn4.style.display = 'none';
+};
+function renderSecondRowButtons() {
+  btn5.style.display = 'inline';
+  btn6.style.display = 'inline';
+  btn7.style.display = 'inline';
+  btn8.style.display = 'inline';
+};
+function removeSecondRowButtons() {
+  btn5.style.display = 'none';
+  btn6.style.display = 'none';
+  btn7.style.display = 'none';
+  btn8.style.display = 'none';
+};
+function halfDayWork() {
+  days += .5;
+  daysLeft -= .5;
+  updateStatus();
+};
+function fullDayWork() {
+  days += 1;
+  daysLeft -= 1;
+  updateStatus();
 };
